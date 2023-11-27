3844e4996ecef7181f6a0e3051a9f7226d359763
aqaqsubin <aqaq925120k@naver.com>
2023-11-27 23:33:58 +0900

    :construction: Update dataset class & gitignore

---
+++ src/data_utils.py
@@ -0,0 +1,28 @@
+import os
+import json
+import errno
+
+from shutil import rmtree
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+def del_folder(path):
+    try:
+        rmtree(path)
+    except:
+        pass
+
+def read_json(json_path):
+    with open(json_path, 'r') as f:
+        return json.load(f)
+
+def write_json(save_path, json_obj):
+    with open(save_path, 'w', encoding='utf-8') as make_file:
+        json.dump(json_obj, make_file, indent="\t")
+    return
---
+++ src/dataset.py
@@ -0,0 +1,59 @@
+import os
+import torch
+import random
+
+import pandas as pd
+
+from PIL import Image
+from os.path import join as pjoin
+from typing import Dict, List, Tuple
+
+from torch.utils.data import Dataset
+from transformers import AutoProcessor
+
+from data_utils import read_json
+
+class CUB_200_2011(Dataset):
+    def __init__(self, 
+                 processor: AutoProcessor,
+                 data_path: str, 
+                 vision_dir: str, 
+                 attributes_path: str=None,
+                 attributes: List[str]=None
+                 ):
+        self.vision_dir = vision_dir
+        self.data = read_json(data_path)
+        self.attributes = read_json(attributes_path) if not attributes else attributes
+            
+        self.processor = processor
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):        
+        num_trial = 10
+        for _ in range(num_trial):
+            try:
+                # Process image
+                image_path = pjoin(self.vision_dir, self.data[index]["image_path"])
+                image = Image.open(image_path).convert("RGB")
+                
+                encoding = self.processor(images=[image for _ in self.attributes],
+                                        text=self.attributes, 
+                                        padding=True, 
+                                        return_tensors="pt")
+                
+                # pixel_values: (bs, n_attr, c, h, w)
+                # qformer_input_ids: (bs, n_attr, l)
+                # label: (bs, n_attr)
+                cls_id = int(self.data[index]["image_path"].split(".")[0])
+                encoding['label'] = torch.tensor(cls_id, dtype=torch.long).repeat(len(self.attributes))
+                return encoding
+
+            except Exception as e:
+                print(f"Failed to load examples: {self.data[index]}. ({e})")
+                index = random.choice(range(len(self.data)))
+        
+        return None
+    
+    
